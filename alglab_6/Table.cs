aa1a9b6eedd8a1d171d8b4202e1284cedb0141fb
Amir <70907990+Amirka2@users.noreply.github.com>
2022-12-22 18:54:10 +0500

    added abstract class for table

---
+++ alglab_6/Table.cs
@@ -0,0 +1,42 @@
+namespace alglab_6;
+
+public abstract class Table<U>
+{
+    protected Item<U>[] _items;
+    protected readonly float _loadFactor = 0.75F;
+    protected int _loaded = 0;
+
+    public Table()
+    {
+        _items = new Item<U>[8];
+    }
+    public Table(int capacity)
+    {
+        _items = new Item<U>[capacity];
+    }
+    public Table(int capacity, float loadFactor)
+    {
+        _loadFactor = loadFactor;
+        _items = new Item<U>[capacity];
+    }
+
+    public abstract bool Add(string key, U data);
+    public abstract bool Remove(string key);
+    
+    protected virtual void ResizeTable()
+    {
+        Item<U>[] items = new Item<U>[_items.Length * 2];
+        for (int i = 0; i < _items.Length; i++)
+        {
+            if (_items[i] is null) continue;
+            var index = GetIndexByHash(_items[i].GetHashCode());
+            items[i] = _items[i];
+        }
+
+        _items = items;
+    }
+    protected int GetIndexByHash(int hash)
+    {
+        return hash % _items.Length;
+    }
+}
