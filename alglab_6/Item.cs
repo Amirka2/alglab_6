e52b5cdc1f841ba578f2c42715425bd91fc2daf6
Amir <70907990+Amirka2@users.noreply.github.com>
2022-12-26 22:21:37 +0500

    добавлены константы и поле индекса

--- alglab_6/Item.cs
+++ alglab_6/Item.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -8,13 +9,25 @@ namespace alglab_6
 	{
 		public string Key { get; private set; }
 		public U Value { get; private set; }
+		private const double GOLDEN_RATIO = 1.61803398;
+		public int Index { get; set; }
+		private const double C1 = 1;
+		private const double C2 = 2;
 
-        public Item(string key, U value)
+		public Item(string key, U value)
         {
 	        if (value == null) throw new ArgumentNullException();
 			Key = key;
 			Value = value;
 		}
+
+        public Item(string key, U value, int index)
+        {
+	        if (value == null) throw new ArgumentNullException();
+	        Key = key;
+	        Value = value;
+	        Index = index;
+        }
         public override bool Equals(object? obj)
         {
             var item = (Item<U>)obj;
@@ -34,6 +47,29 @@ namespace alglab_6
 
         public byte[] GetHash()
         {
+	        // var x1 = HashF(); //двойное хэширование
+	        // var x2 = GetOwnHash(0);
+	        // for (int i = 0; i < Math.Min(x1.Length, x2.Length); i++)
+	        // {
+		       //  x1[i] += x2[i];
+	        // }
+	        //
+	        // return x1;
+	        
+	        // return HashF(); //линейное
+
+	        var x1 = BitConverter.GetBytes(Index * C1 + Math.Pow(Index, 2) * C2); //квадратичное
+	        var x2 = HashF();
+	        for (int i = 0; i < Math.Min(x1.Length, x2.Length); i++)
+	        {
+		        x1[i] += x2[i];
+	        }
+
+	        return x1;
+        }
+
+        private byte[] HashF()
+        {
 	        byte[] res;
 	        // Creates an instance of the default implementation of the MD5 hash algorithm.
 	        using (var md5Hash = MD5.Create())
@@ -47,6 +83,23 @@ namespace alglab_6
 	        }
 
 	        return res;
+        }
+
+        private byte[] GetOwnHash(int option)
+        {
+	        switch (option)
+	        {
+		        case 0:
+			        return BitConverter.GetBytes(Math.Abs(10000 * (Key.GetHashCode() * GOLDEN_RATIO % 1))); //сделать переменную размер таблмцы для скринов
+			        break;
+		        case 1:
+			        return BitConverter.GetBytes(Math.Abs(Key.GetHashCode() % 10000));
+			        break;
+		        default:
+			        break;
+	        }
+
+	        return null;
         }
     }
 }
